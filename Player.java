3c4dbe3666fa4fd31bbc446e5bfa640b2b3074ec
mdw223 <151095055+mdw223@users.noreply.github.com>
2023-11-22 10:48:51 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,37 @@
+import java.util.*; // utilizing Scanner
 
+/**
+ * This class represents a blueprint for a Player object and assigns them
+ * a name and designated piece: X or O
+ */
+public class Player {
+
+    /** instance of object: representing name of player */
+    private String name;
+    /** instance of object: representing piece */
+    private String piece;
+    
+    /**
+     * Constructor for Player object
+     */
+    public Player(String name, String piece) {
+        this.name = name;
+        this.piece = piece;
+    }
+
+    /**
+     * Accessor for name: instance of player object
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Accessor for piece: instance of player object 
+     */
+    public String getPiece() {
+        return piece;
+    }
+
+
+}
